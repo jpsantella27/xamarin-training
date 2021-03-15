0ca32e5e19f15bd918713593cee21014d1003736
Jon Hero Santella <jonhero.santella@gmail.com>
2021-03-15 19:13:34 +0800

    [JPS] XamIntro SC

---
+++ XamIntro/XamIntro/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamIntro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new RegisterPage();
+            // MainPage = new BookingsCarouselPage();
+            MainPage = new BookingsTabbedPage();
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamIntro/XamIntro/RegisterPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamIntro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if(Device.Idiom != TargetIdiom.Phone)
+            {
+               
+            }
+        }
+
+        public void ButtonRegister_Clicked(object sender, EventArgs e)
+        {
+            var uname = EntryUserName.Text;
+            var pwd = EntryPassword.Text;
+            var email = EntryEmailAddress.Text;
+            var mobile = EntryMobileNumber.Text;
+
+            var msg = $"User name: {uname}\nPassword : {pwd}\nEmail Address: {email}\nMobile: {mobile}";
+
+            DisplayAlert("Register", msg, "OK");
+        }
+
+        public void ButtonClear_Clicked(object sender, EventArgs e)
+        {
+            EntryUserName.Text = string.Empty;
+            EntryPassword.Text = string.Empty;
+            EntryEmailAddress.Text = string.Empty;
+            EntryMobileNumber.Text = string.Empty;
+        }
+    }
+}
